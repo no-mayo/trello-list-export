c8614f6bc2b1b7876306829deb62995b012934f6
Louise Swift <louise@louiseswift.com>
2017-10-27 14:08:22 +0100

    Basic points counting

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
   var $cardsResult = $(".cards-result");
   var $listIds = $(".list-ids");
   var $loaderBar = $(".loader .bar");
+  var $pointsTableContaier = $("#pointsTableContainer");
 
   $(".js-auth-with-trello").on("click", function() {
     window.Trello.authorize({
@@ -72,13 +73,21 @@ $(document).ready(function() {
     var listCount = 0;
     var listIds = $listIds.val();
     var includeChecklists = $("#includeChecklists")[0].checked;
+    var countPoints = $("#countPoints")[0].checked;
+    var listPoints = {};
 
     // reset the .cards-result field & loader bar ready for a new result
     $cardsResult.val("Fetching cards...");
+    $pointsTableContaier.html("");
     updateLoader(0, 0);
 
     if (listIds) {
       listIds.forEach(function(listId) {
+        // if we're counting points, add entry to listPoints for this list
+        if (countPoints) {
+          listPoints[listId] = {};
+        }
+
         Trello.get("/lists/" + listId + "/cards")
           .then(function(fetchedCards) {
             // keep track of how many lists we've fetched cards from
@@ -88,6 +97,7 @@ $(document).ready(function() {
             fetchedCards.forEach(function(fetchedCard) {
               cardsToDisplay[fetchedCard.id] = {
                 idChecklists: fetchedCard.idChecklists,
+                idList: listId,
                 name: fetchedCard.name,
                 pos: fetchedCard.pos
               };
@@ -99,12 +109,15 @@ $(document).ready(function() {
               // items that it has)
               var cardIds = Object.keys(cardsToDisplay);
               var cardCount = 0;
+              var cardPointCount = 0;
 
               cardIds.forEach(function(cardId) {
                 var card = cardsToDisplay[cardId];
 
                 card.displayString = `* ${card.name}\r\n`;
 
+                // check for any checklists (regardless of the user options
+                // selected, because otherwise we end up in a weird async place)
                 Trello.get("/cards/" + cardId + "/checklists")
                   .then(function(checklists) {
                     var checkItems = flatten(
@@ -113,6 +126,7 @@ $(document).ready(function() {
                       )
                     );
 
+                    // add the checklist items if user selected that option
                     if (includeChecklists) {
                       card.displayString += checkItems
                         .map(name => `  - ${name}\r\n`)
@@ -135,6 +149,31 @@ $(document).ready(function() {
                   .error(function(err) {
                     console.log("error:", err);
                   });
+
+                // count card points by card member if user selected that option
+                if (countPoints) {
+                  Trello.get("/cards/" + cardId + "/members")
+                    .then(function(members) {
+                      // keep track of how many cards' points we've checked
+                      cardPointCount++;
+
+                      members.map(m => m.fullName).forEach(name => {
+                        let points = cardPoints(card.name);
+                        if (listPoints[card.idList][name]) {
+                          listPoints[card.idList][name] += points;
+                        } else {
+                          listPoints[card.idList][name] = points;
+                        }
+                      });
+
+                      if (cardPointCount == cardIds.length) {
+                        displayPoints(listPoints);
+                      }
+                    })
+                    .error(function(err) {
+                      console.log(err);
+                    });
+                }
               });
             }
           })
@@ -159,6 +198,41 @@ $(document).ready(function() {
     });
 
     $cardsResult.val(cardsArray.map(card => card.displayString).join(""));
+  }
+
+  function displayPoints(pointsByList) {
+    // build a row for each list in pointsByList, containing child rows for each
+    // card member (person) who has points within each list
+    let rows = "";
+
+    for (listId in pointsByList) {
+      // to start, get the list name from the lists <select> dropdown in the view
+      // (#lazy)
+      let listName = $(`.list-ids option[value="${listId}"]`).text();
+      let listMembers = pointsByList[listId];
+
+      rows += `
+        <tr>
+          <td>${listName}</td>
+          <td>
+            <table>`;
+
+      for (name in listMembers) {
+        let points = listMembers[name];
+
+        rows += `
+              <tr>
+                <th>${name}</th><td>${points}</td>
+              </tr>
+        `;
+      }
+
+      rows += "</table></td></tr>";
+    }
+
+    $("#pointsTableContainer").html(
+      "<h3>Points by List & Card Member</h3><table>" + rows + "</table>"
+    );
   }
 });
 
@@ -202,6 +276,23 @@ function updateBoardsList(opts) {
     .error(function(err) {
       console.log(err);
     });
+}
+
+function cardPoints(name) {
+  let points = 0;
+  let bracketMatches = name.match(/([^[]+?)(?=\])/g); // e.g. [5] or [SPIKE-2]
+
+  if (bracketMatches) {
+    bracketMatches.forEach(match => {
+      // strip non-numeric
+      let num = parseInt(match.replace(/\D/g, ""), 10);
+      if (!isNaN(num)) {
+        points += num;
+      }
+    });
+  }
+
+  return points;
 }
 
 function uniq(a) {
